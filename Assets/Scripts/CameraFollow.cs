b7ef57996afe7c1bc2eb5dda6f1aa6ead9311175
Wasut <wasut_pao@hotmail.com>
2016-12-01 22:45:32 +0700

    add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform player;	//Target to follow;
+	public float xPositionOffset;
+
+	private float playerXPosition;
+	private float cameraXPosition;
+
+	void Start(){
+		cameraXPosition = transform.position.x;
+	}
+
+	void Update(){
+		playerXPosition = player.transform.position.x - xPositionOffset;  //Position of the player with calculate offset
+
+		if (playerXPosition > cameraXPosition) {
+			cameraXPosition = playerXPosition;
+		}
+			
+		moveCamera(cameraXPosition);
+	}
+
+	void moveCamera(float xPosition){
+		transform.position = new Vector3 (xPosition, transform.position.y , transform.position.z); 
+	}
+}
