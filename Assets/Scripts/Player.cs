6a7bbfd13490907ec0c9ecf2fdc66e40a13e529d
Wasut <wasut_pao@hotmail.com>
2016-12-01 22:33:32 +0700

    add Player script with jump method

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour {
+
+	[Header("Movement of the player")]
+	public float jumpHeight;
+	public float forwardSpeed;
+
+	private Rigidbody2D mainRigidbody2D;
+
+	void Start()
+	{
+		mainRigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))){
+			Jump();
+		}
+	}
+	
+	void Jump(){
+		mainRigidbody2D.velocity = new Vector2(forwardSpeed,jumpHeight);
+	}
+}
