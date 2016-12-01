c7d9c49b97e88023f18a51a01157f649fa28615c
Wasut <wasut_pao@hotmail.com>
2016-12-02 00:02:37 +0700

    add script for control button

---
+++ Assets/Scripts/Game_ButtonFunctions.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_ButtonFunctions : MonoBehaviour {
+	
+	public AudioClip clickSound;
+
+	void Update(){
+		//If touch back on mobile and in INGAME state
+		if (Input.GetKeyDown (KeyCode.Escape) && GameManager.instance.currentState == GameStates.INGAME) { 
+			GameManager.instance.Pause();
+		}
+
+		//If touch back on mobile and in PREGAME state
+		if (Input.GetKeyDown (KeyCode.Escape) && GameManager.instance.currentState == GameStates.PREGAME) { 
+			#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false; //Stop UnityEditor
+			#else
+				Application.Quit();
+			#endif
+		}
+	}
+
+	public void OnClick(string buttonName){
+
+		SoundManager.instance.PlaySingleSoundEffect(clickSound);
+
+		switch (buttonName) {
+			case "Restart" :
+					Time.timeScale = 1;
+					Application.LoadLevel(Application.loadedLevel);
+					break;
+
+			case "Pause" :
+				GameManager.instance.Pause();
+				break;
+
+			case "Resume" :
+				GameManager.instance.Resume();
+				break;
+
+			case "Rate" :
+				//Put your URL
+				Application.OpenURL ("https://play.google.com/store/apps/details?id=com.example"); 
+				break;
+				
+			case "MuteMusic" :
+				SoundManager.instance.MuteMusic(); 
+				break;
+				
+			case "MuteSoundEffect" :
+				SoundManager.instance.MuteSoundEffect ();
+				break;
+		}
+	}
+}
