d2e2342faaf721f1e6503d6655d99aeff2d6b598
Wasut <wasut_pao@hotmail.com>
2016-12-01 22:53:52 +0700

    add obstacle spawner script

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour {
+	public static ObstacleSpawner instance;
+
+	[Header("Obstacle")]
+	public GameObject obstacle;
+
+	[Header("Spawn Position")]
+	[Tooltip("For random y position of obstacle")]
+	public float rangeYPosition;
+	[Tooltip("Distance between obstacle")]
+	public float distanceXPosition;
+
+	private float lastedXPosition;
+
+	void Awake () {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		initialObstacles (2);
+	}
+
+	public void initialObstacles(int amount){
+		for(int i = 0 ; i < amount ; i++){
+			spawnObstacle();
+		}
+	}
+	
+	public void spawnObstacle(){
+		float yPosition = Random.Range(-rangeYPosition, rangeYPosition);
+		float xPosition = lastedXPosition += distanceXPosition;
+
+		Vector3 spawnPosition = new Vector3 (xPosition, yPosition, 0.0f);
+
+		Instantiate (obstacle, spawnPosition , Quaternion.identity); //Create obstacle to game
+	}
+}
