467d6187e3db7205916632eb54afcf1ebae9adca
Wasut <wasut_pao@hotmail.com>
2016-12-01 23:06:19 +0700

    add Obstacle script for delete obstacle when out of screen

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		destroyWhenOutOfScreen ();
+	}
+
+	private void destroyWhenOutOfScreen(){
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+		//When obstacle out of screen
+		if (screenPosition.x < -1){
+			Destroy(gameObject);
+		}
+	}
+}
